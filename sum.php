aa4c34c3fa6f82d9729ba6b6f3bbf5bca93df4dd
kyphoenix <40657484+kyphoenix@users.noreply.github.com>
2021-03-05 10:41:59 +0300

    Add files via upload

---
+++ sum.php
@@ -0,0 +1,12 @@
+<?php
+/**
+*
+*it will return the sum of two integers
+*The first number
+*The second number
+*The sum of the two numbers
+*/
+
+function add($x, $y){
+    return $x + $y;
+}
---
+++ tests/functiontest.php
@@ -0,0 +1,14 @@
+<? php
+
+
+
+class functiontest extends PHPUnit_Framework_TestCase
+{
+    public function sumReturnsTheCorrectValue()
+    {
+        require 'sum.php';
+        $this->assertEquals(4, add(2,2));
+        $this->assertEquals(8, add(2,6));
+
+    }
+}
