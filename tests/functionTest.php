77a96fd8f526509e94fe1b276d85572c59a5f4d5
kyphoenix <40657484+kyphoenix@users.noreply.github.com>
2021-03-05 11:42:20 +0300

    Update functionTest.php

--- tests/functionTest.php
+++ tests/functionTest.php
@@ -6,7 +6,7 @@ class functiontest extends PHPUnit_Framework_TestCase
 {
     public function sumReturnsTheCorrectValue()
     {
-        require 'sum.php';
+        require 'src/sum.php';
         $this->assertEquals(4, add(2,2));
         $this->assertEquals(8, add(2,6));
 
